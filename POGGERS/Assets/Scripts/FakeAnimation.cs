ad568c0acecab9ce249e71480a5a9fe03d39c541
Isaac <isacmen@gmail.com>
2018-06-13 01:52:09 -0700

    put in "touch" controls, added some art
    all the new shit is in "Isaac2" scene
    also updated playercontroller is "PlayerController2"

---
+++ POGGERS/Assets/Scripts/FakeAnimation.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeAnimation : MonoBehaviour {
+
+	public Sprite LeftAttack1;
+	public Sprite LeftAttack2;
+	public Sprite RightAttack1;
+	public Sprite RightAttack2;
+	public Sprite CenterAttack1;
+	public Sprite CenterAttack2;
+	public Sprite LeftMove;
+	public Sprite RightMove;
+	public Sprite Block;
+	public Sprite Idle;
+
+	private SpriteRenderer renderer;
+	private PlayerController2 player;
+	private string nextanim;
+
+	private int timer;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<SpriteRenderer> ();
+		player = GetComponentInParent<PlayerController2> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.getCurrentActionString () == "None") {
+			ChangeSprite ();
+			timer++;
+		}
+		if (timer > 45) {
+			this.transform.localScale = new Vector3(4.0f, 4.0f, 1.0f);
+			renderer.sprite = Idle;
+			timer = 0;
+			nextanim = "";
+		}
+		PrepSprite ();
+	}
+
+	void PrepSprite () {
+		if (player.getCurrentActionString () == "Left Attack") {
+			renderer.sprite = LeftAttack1;
+			nextanim = "LeftAttack";
+		}
+		if (player.getCurrentActionString () == "Straight Attack") {
+			renderer.sprite = CenterAttack1;
+			nextanim = "StraightAttack";
+		}
+		if (player.getCurrentActionString () == "Right Attack") {
+			this.transform.localScale = new Vector3(-4.0f, 4.0f, 1.0f);
+			renderer.sprite = RightAttack1;
+			nextanim = "RightAttack";
+		}
+		if (player.getCurrentActionString () == "Move Left") {
+			renderer.sprite = LeftMove;
+			nextanim = "LeftMove";
+		}
+		if (player.getCurrentActionString () == "Move Right") {
+			this.transform.localScale = new Vector3(-4.0f, 4.0f, 1.0f);
+			renderer.sprite = RightMove;
+			nextanim = "RightMove";
+		}
+		if (player.getCurrentActionString () == "Block") {
+			nextanim = "Block";
+		}
+	}
+
+	void ChangeSprite () {
+		if (nextanim == "LeftAttack") {
+			renderer.sprite = LeftAttack2;
+		}
+		if (nextanim == "StraightAttack") {
+			renderer.sprite = CenterAttack2;
+		}
+		if (nextanim == "RightAttack") {
+			renderer.sprite = RightAttack2;
+		}
+		if (nextanim == "LeftMove") {
+			renderer.sprite = LeftMove;
+		}
+		if (nextanim == "RightMove") {
+			renderer.sprite = RightMove;
+		}
+		if (nextanim  == "Block") {
+			renderer.sprite = Block;
+		}
+	}
+}
---
+++ POGGERS/Assets/Scripts/Input/PlayerController2.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : CharacterController {
+
+	private float initialX;
+	private float currentX;
+	private float Xdirection;
+
+	private float initialY;
+	private float currentY;
+	private float Ydirection;
+
+	// Use this for initialization
+	void Start ()
+	{
+		hp = 3;
+		atk = 1;
+
+		currentPosition = CharacterPosition.Middle;
+		currentAction = CharacterAction.None;
+
+		sprite = GetComponent<SpriteRenderer>();
+
+		moveLocked = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!moveLocked)
+		{
+			
+
+			#region Left Move Input
+			// ==========================================
+			// |                                        |
+			// |            Left Move Input             |
+			// |                                        |
+			// ==========================================
+
+			if (ReturnDirection () == "Move-Left")
+			{
+				ResetDirection();
+				if (currentPosition != CharacterPosition.Left)
+				{
+					currentAction = CharacterAction.MoveLeft;
+
+					moveLocked = true;
+
+				}
+			}
+			#endregion Left Move Input
+
+			#region Right Move Input
+			// ==========================================
+			// |                                        |
+			// |            Right Move Input            |
+			// |                                        |
+			// ==========================================
+
+			if (ReturnDirection () == "Move-Right")
+			{
+				ResetDirection();
+				if (currentPosition != CharacterPosition.Right)
+				{
+					currentAction = CharacterAction.MoveRight;
+
+					moveLocked = true;
+
+				}
+			}
+			#endregion Right Move Input
+
+			#region Block Input
+			// ==========================================
+			// |                                        |
+			// |               Block Input              |
+			// |                                        |
+			// ==========================================
+
+			if (ReturnDirection () == "Block")
+			{
+				ResetDirection();
+				// Sets action to block
+				currentAction = CharacterAction.Block;
+
+				moveLocked = true;
+
+			}
+			#endregion Block Input
+
+			#region Left Attack Input
+			// ==========================================
+			// |                                        |
+			// |           Left Attack Input            |
+			// |                                        |
+			// ==========================================
+
+			// Checks if that position is not on the left as well
+			if (ReturnDirection() == "Attack-Left" && currentPosition != CharacterPosition.Left)
+			{
+				ResetDirection();
+				// Sets action to attack left
+				currentAction = CharacterAction.AttackLeft;
+
+				moveLocked = true;
+
+			}
+			#endregion Left Attack Input
+
+			#region Straight Attack Input
+			// ==========================================
+			// |                                        |
+			// |         Straight Attack Input          |
+			// |                                        |
+			// ==========================================
+
+			if (ReturnDirection () == "Attack-Center")
+			{
+				ResetDirection();
+				// Sets action to attack left
+				currentAction = CharacterAction.AttackStraight;
+
+				moveLocked = true;
+
+			}
+			#endregion Straight Attack Input
+
+			#region Right Attack Input
+			// ==========================================
+			// |                                        |
+			// |          Right Attack Input            |
+			// |                                        |
+			// ==========================================
+
+			// Checks if that position is not on the right as well
+			if (ReturnDirection () == "Attack-Right" && currentPosition != CharacterPosition.Right)
+			{
+				ResetDirection();
+				// Sets action to attack right
+				currentAction = CharacterAction.AttackRight;
+
+				moveLocked = true;
+
+			}
+			#endregion Right Attack Input
+		}
+	}
+
+
+	// V V MOUSE DRAG INPUT V V
+	void OnMouseDown() {
+		initialX = Input.mousePosition.x;
+		initialY = Input.mousePosition.y;
+	}
+
+	void OnMouseUp() {
+		currentX = Input.mousePosition.x;
+		Xdirection = initialX - currentX;
+
+		currentY = Input.mousePosition.y;
+		Ydirection = initialY - currentY;
+
+		Debug.Log(ReturnDirection ());
+	}
+
+	string ReturnDirection() {
+		string result;
+
+		if (Ydirection < -100) {
+			if (Xdirection > 180){
+				result = "Attack-Left";
+				return result;
+			} else if (Xdirection < -180) {
+				result = "Attack-Right";
+				return result;
+			} else {
+				result = "Attack-Center";
+				return result;
+			}
+		} else if (Ydirection > 180) {
+			result = "Block";
+			return result;
+		} else if (Mathf.Abs (Xdirection) > 100) {
+			if (Xdirection > 100) {
+				result = "Move-Left";
+				return result;
+			} else if (Xdirection < -100) {
+				result = "Move-Right";
+				return result;
+			}
+		}
+
+		result = "error";
+		return result;
+	}
+
+	void ResetDirection () {
+		initialX = 0;
+		initialY = 0;
+		Xdirection = 0;
+
+		currentX = 0;
+		currentY = 0;
+		Ydirection = 0;
+
+		Debug.Log ("reset");
+	}
+}
